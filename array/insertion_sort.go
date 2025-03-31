a7f2b6869285dc00b3a153d7b7ef7bee4e2d53f6
kam05 <kamalsolanki05@gmail.com>
2025-03-31 17:36:15 +0530

    insertion sort fixed

--- array/insertion_sort.go
+++ array/insertion_sort.go
@@ -9,11 +9,15 @@ func main() {
 func insertionSort(arr []int) []int {
 
 	for i := 1; i < len(arr); i++ {
-		for j := 0; j < i; j++ {
-			if arr[i] < arr[j] {
-				arr[i], arr[j] = arr[j], arr[i]
-			}
+		key := arr[i]
+		j := i - 1
+
+		for j >= 0 && arr[j] > key {
+			arr[j+1] = arr[j]
+			j--
 		}
+		arr[j+1] = key
 	}
+
 	return arr
 }
